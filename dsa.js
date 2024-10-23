5c3763b906cef3aeab9ac8f776cc7bc9c0b79bcd
Deepak Gupta <deepakgupta60k@gmail.com>
2024-10-24 01:06:41 +0530

    fourth session

---
+++ dsa.js
@@ -0,0 +1,83 @@
+// #### Session 4 String
+
+// What?
+  
+// Array of character, can contain alphanumeric characters, special character.
+
+// lots of similarly with array
+// concept of indexing
+// common type : slice, indexOf(), lastIndexOf(), split(), slice()
+// common terminologies 
+
+
+// Substring vs Subsequence
+
+// Contiguous part of string is a substring, 
+  
+// Subsequence: can't change order -> so that is subsequence
+
+(function main(){
+  
+  
+  
+    let arr = ["hello", "world"]
+    console.log(arr.join(" ")) // join is to join the 
+     
+      
+      // create array with empty value..
+      
+      
+      
+      
+      
+      // let str = "hello world, my name is deepak";
+      // let arr = str.split("");
+      
+      // let str=" hello world "
+      
+      // let arr = str.split("")
+      // console.log(str)
+    
+      // arr = arr.filter(i=>i!=" ")
+      // str = str.replaceAll(" ","")
+      
+    //   let res=[]
+    //   for(let i=0;i<arr.length;i++)
+    //     {
+    //       if(arr[i]!==" ")
+    //         {
+    //           res.push(arr[i])
+    //         }
+    
+    //     }
+      
+      
+      // console.log(arr)
+      // let arr ="hello world"
+      // console.log(arr.lastIndexOf(""))
+      // let str = "hello world"
+      // let arr = str.split("");
+      // let res = []
+      // for(let i=0;i<arr.length;i++)
+      //   {
+          
+      //     console.log(str[i])
+      //     // if(arr[i]==" ")
+      //     //   {
+      //     //     res.push(arr[i])
+      //     //   }
+      //   }
+    
+      
+      // let arr =["hello", "world"]
+    
+    // let arr =str.trim();
+      
+    // let arr = str.filter(i=> i!==" ")
+    // console.log(arr.join(" "));
+    // let arr = str.replace(" ", "");
+      // console.log(arr)
+      
+      // string is remove first and last space of the character
+      
+    }())
