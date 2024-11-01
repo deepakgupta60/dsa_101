218f40dce66a5ab9be22a9dc97f9fdcca367c515
Deepak Gupta <deepakgupta60k@gmail.com>
2024-11-01 19:12:12 +0530

    working

--- anagram.js
+++ anagram.js
@@ -23,3 +23,45 @@
         return "false"
       }
   }
+
+
+  (function main(){
+  
+    let ans = findAnag()
+    
+    console.log("Answer is: ", ans)
+  
+  }())
+  
+  function anagram(s1,s2)
+  {  
+    if(s1.length!=s2.length) return false
+    
+    s1=s1.split("").sort().join("")
+    s2=s2.split("").sort().join("")
+    
+    if(s1==s2)
+      {
+        return true
+      }
+    else
+      {
+        return false
+      }
+    
+  }
+  
+  function findAnag()
+  {
+  
+    let arr=["cba","abc","bca","pka"]
+    let res=[]
+    for(let i=0;i<arr.length;i++)
+    {
+      if(anagram(arr[0], arr[i])==true)
+      {
+        res.push(arr[i])
+      }
+    }
+    return res.sort()
+  }
